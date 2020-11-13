6e7ef4d6201c1d54f215e8e2865fed398c86db99
Remi Schwartz <schwartz.remi@gmail.com>
2020-11-13 18:59:40 +0100

    added reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @dose = Dose.new
+      render 'cocktails/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
+end
